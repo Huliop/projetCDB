50043ebc39cb94c260bdaa3f2dee88652ec6274a
huliop <baptistesaladin@gmail.com>
2019-11-18 18:01:47 +0100

    remise en marche de la console

--- console/src/main/java/console/MainConfig.java
+++ console/src/main/java/console/MainConfig.java
@@ -1,14 +1,19 @@
 package console;
 
+import java.util.Properties;
+
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
+@Configuration
 @ComponentScan(basePackages = { "service", "persistence", "console" })
 @PropertySource(value = "classpath:application.properties")
 public class MainConfig {
@@ -17,12 +22,34 @@ public class MainConfig {
 	private Environment env;
 
 	@Bean
-	 public DataSource getConnection() {
-		 DriverManagerDataSource dataSource = new DriverManagerDataSource();
-		 dataSource.setDriverClassName(env.getProperty("dataSource.driverClassName"));
-		 dataSource.setUrl(env.getProperty("dataSource.jdbcUrl"));
-		 dataSource.setUsername(env.getProperty("dataSource.user"));
-		 dataSource.setPassword(env.getProperty("dataSource.password"));
-		 return dataSource;
-	 }
+	public DataSource getConnection() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		dataSource.setUrl(env.getProperty("jdbc.url"));
+		dataSource.setUsername(env.getProperty("jdbc.user"));
+		dataSource.setPassword(env.getProperty("jdbc.password"));
+		return dataSource;
+	}
+
+	@Bean
+	public LocalSessionFactoryBean sessionFactory() {
+		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+		sessionFactory.setDataSource(getConnection());
+		sessionFactory.setPackagesToScan(new String[] { "core.model" });
+		sessionFactory.setHibernateProperties(hibernateProperties());
+
+		return sessionFactory;
+	}
+	
+	Properties hibernateProperties() {
+		return new Properties() {
+			private static final long serialVersionUID = 1L;
+
+			{
+				setProperty("hibernate.hbm2ddl.auto", env.getProperty("hibernate.hbm2ddl.auto"));
+				setProperty("hibernate.dialect", env.getProperty("hibernate.dialect"));
+				setProperty("hibernate.globally_quoted_identifiers", "true");
+			}
+		};
+	}
 }
